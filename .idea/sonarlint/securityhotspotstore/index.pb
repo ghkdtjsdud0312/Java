87b372e6ff142b60eae4d1062098464ef1da80b6
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-08-20 23:33:16 +0900

    test commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+I
+src/Chapter02/ex2_01.java,8/c/8c28b3c67a738c21ae739eb02c1becfbd9f7eed4
---
+++ src/Chapter02/ex2_01.java
@@ -0,0 +1,10 @@
+package src.Chapter02;
+
+public class ex2_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+    }
+}
