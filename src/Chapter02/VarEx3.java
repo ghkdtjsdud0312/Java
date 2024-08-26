65cf0e719f75c49d0e4e7a1084dd5a2543e41eb5
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-08-26 21:28:26 +0900

    varex3 예제추가

--- src/Chapter02/VarEx3.java
+++ src/Chapter02/VarEx3.java
@@ -24,5 +24,21 @@ public class VarEx3 {
         System.out.println(.10);
         System.out.println(10f);
         System.out.println(1e3);
+
+        char ch = 'A'; // ''에는 하나의 문자만 들어가야 함
+        int i = 'A';
+        System.out.println(ch);
+        System.out.println(i);
+
+        String str = ""; // 빈 공백도 괜찮음
+        String str2 = "ABCD";
+        String str3 = "123";
+        String str4 = str2 + str3;
+        System.out.println(str);
+        System.out.println(str2);
+        System.out.println(str3);
+        System.out.println(str4);
+        System.out.println(""+7+7); // 77
+        System.out.println(7+7+""); // 14
     }
 }
